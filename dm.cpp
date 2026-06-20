7baa48993d39f015247dfb1a9d9a5489c999d9fa
moneesh2601d <eeshwar2601d@gmail.com>
2026-06-20 16:19:52 +0530

    Add missing namespace for __gnu_pbds

--- dm.cpp
+++ dm.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-using namespace __gnu_pbds;
 
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 
 typedef tree< int, null_type,
            less<int>,rb_tree_tag,tree_order_statistics_node_update> st;
